06b90b87553d66bb49b2ea882dc9ea422f2c5166
serhatck <serhatcetinkayaa@gmail.com>
2015-12-10 17:51:08 +0200

    Update GameOverPanel.java

--- ProjectCodes/GameOverPanel.java
+++ ProjectCodes/GameOverPanel.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 public class GameOverPanel extends JPanel{
 	
 	public GameOverPanel(int dim1, int dim2){
@@ -10,8 +9,28 @@ public class GameOverPanel extends JPanel{
                 JPanel panel = new JPanel();
                 JLabel title = new JLabel("GAME OVER !");
                 title.setFont(new Font("Buxton Sketch", Font.PLAIN, 40));
+<<<<<<< HEAD
+                title.setBackground(Color.GRAY);
+                
+                StringBuilder sb = new StringBuilder(500);
+                sb.append("<html><br></br>").
+                        append("The Game is over.").
+                        append("<br></br>").
+                        append("<br>Your Score is:</br>");
+                ///////High Score will be shown here
+                ///////code will be updated
+                JLabel gameOver = new JLabel(sb.toString());
+                gameOver.setFont(new Font("Buxton Sketch", Font.PLAIN, 18));
+                panel.setPreferredSize(new Dimension(dim1,dim2));
+		panel.add(title);
+		panel.add(gameOver);
+                add(panel, BorderLayout.CENTER);
+		setPreferredSize(new Dimension(dim1,dim2));
+		setBackground(Color.GRAY);
+=======
                 title.setBackGround(Color.GRAY);
                 
                 
+>>>>>>> 3bc87a3369e0a2d14174f0efd8cb15166c798f09
 	}
 }
