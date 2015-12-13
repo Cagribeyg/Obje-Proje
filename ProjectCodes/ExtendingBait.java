62fbcc967f556434decef0b8fb860f04e0ee11ee
Cagribeyg <cagribeyg@gmail.com>
2015-12-13 04:49:51 -0800

    Extending Update

--- ProjectCodes/ExtendingBait.java
+++ ProjectCodes/ExtendingBait.java
@@ -1,5 +1,24 @@
-package snake;
-
-public class ExtendingBait {
-
+public class ExtendingBait extends Bait{
+  
+  public ExtendingBait(Piece piece){
+    
+    super.color = "Blue";
+    super.snake = null;
+    super.baitPiece = piece;
+    
+  }
+  
+  @Override 
+  public void affect(){
+  
+  }
+  
+  @Override
+  public void setSnake(Snake snake){
+	super.snake = snake;  
+  }
+  @Override
+  public Piece retrievePiece() {
+      return baitPiece;
+  }
 }
